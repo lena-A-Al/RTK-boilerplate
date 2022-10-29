2a01b8c750e9bc2b1be600a68f5de35d7de08ee5
Lena Al <lena.al.developer@gmail.com>
2022-10-29 04:20:13 -0700

    using form and onSubmit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Game from './components/Game';
 import { updateName } from './features/playerSlice';
 import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
+  //states
+  const [name, setName] = useState('');
   //customs hooks:
   const dispatch = useDispatch();
   //selector
@@ -12,15 +14,24 @@ function App() {
   useEffect(() => {
     //grab out username from localStorage
     const username = localStorage.getItem('clicker_username');
+    setName(username);
     dispatch(updateName(username));
   });
 
   //handlers:
   const handlerNameChange = (event) => {
-    console.log(updateName(event.target.value));
-    dispatch(updateName(event.target.value));
-    //using localStorage to store our data without using databases whenever we write name.
-    localStorage.setItem('clicker_username', event.target.value);
+    // console.log(updateName(event.target.value));
+    // dispatch(updateName(event.target.value));
+    // //using localStorage to store our data without using databases whenever we write name.
+    // localStorage.setItem('clicker_username', event.target.value);
+
+    setName(event.target.value);
+  };
+
+  const handleSubmitName = (event) => {
+    event.preventDefault();
+    dispatch(updateName(name));
+    localStorage.setItem('clicker_username', name);
   };
 
   //VIEWS:
@@ -29,12 +40,14 @@ function App() {
       <header className="App-header">
         <h1>Player Name: {playerName}</h1>
         {/* this is going to be a user input data */}
-        <input
-          // name={playerName}
-          type="text"
-          value={playerName}
-          onChange={handlerNameChange}
-        ></input>
+        <form onSubmit={handleSubmitName}>
+          <input
+            // name={playerName}
+            type="text"
+            value={playerName}
+            onChange={handlerNameChange}
+          ></input>
+        </form>
         <Game />
       </header>
     </div>
